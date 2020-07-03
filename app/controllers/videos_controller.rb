aa79c96f7e1ddf9defe06a13573cce52144e675c
Justin <jmarsh24@gmail.com>
2020-07-03 19:57:14 +0200

    Add welcome index

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,8 @@ class VideosController < ApplicationController
   def index
     @video = Yt::Video.new id: 's6iptZdCcG0'
     @playlist = Yt::Playlist.new id:  'PLVpQEq02oKDh6u5QaHibFHWfsoVfYrZuB'
+    @channel = Yt::Channel.new id: 'UCtdgMR0bmogczrZNpPaO66Q'
+    
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 root to: "videos#index"
   resources :videos
+get "/welcome/index"
 end
