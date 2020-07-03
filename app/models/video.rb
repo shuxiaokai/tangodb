ca9605f4657ef7b80d06dd2a1f6fae9387560de8
Stephane Thomas <stephane_t@apple.com>
2020-07-03 14:04:02 -0700

    Add validation to get only good videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,11 @@
 class Video < ApplicationRecord
+  DANCER_SEPARATOR = " and "
+  SONG_SEPARATOR = %r( [–-] )
+
+  validates :leader, presence: true
+  validates :follower, presence: true
+  validates :song, presence: true
+
   class << self
     # To fetch video, run this from the console:
     # Video.for_channel('UCtdgMR0bmogczrZNpPaO66Q')
@@ -15,12 +22,13 @@ class Video < ApplicationRecord
   def parse_title(title)
     # TODO: ensure that the title has the right format
     # follower and leader - song
-    parsed_title = title.split(" – ")
+    return unless title.match(/.+#{DANCER_SEPARATOR}.+#{SONG_SEPARATOR}.+/)
+    parsed_title = title.split(SONG_SEPARATOR)
 
     dancers = parsed_title.first
     self.song = parsed_title.last
 
-    parsed_dancers = dancers.split(" and ")
+    parsed_dancers = dancers.split(DANCER_SEPARATOR)
     self.follower = parsed_dancers.first
     self.leader = parsed_dancers.last
   end
