1af1d3f948a4ad2f0933235b11e11f66a708c0d5
Stephane Thomas <stephane_t@apple.com>
2020-07-05 14:31:26 -0700

    Update leader

--- db/migrate/20200705212132_create_leaders.rb
+++ db/migrate/20200705212132_create_leaders.rb
@@ -1,12 +1,9 @@
 class CreateLeaders < ActiveRecord::Migration[6.0]
   def change
     create_table :leaders do |t|
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :name, null: false, unique: true
 
       t.timestamps null: false
     end
-
-    add_index :leaders, %i[first_name last_name], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,9 @@ ActiveRecord::Schema.define(version: 2020_07_05_212132) do
   enable_extension "plpgsql"
 
   create_table "leaders", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["first_name", "last_name"], name: "index_leaders_on_first_name_and_last_name", unique: true
   end
 
   create_table "videos", force: :cascade do |t|
