5597807969fadfc3e0b787978c6f8a8cd49d0ffc
Justin <jmarsh24@gmail.com>
2020-07-09 17:55:46 +0200

    Fix sorting with default in main table

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,18 +1,21 @@
 class VideosController < ApplicationController
 
+  helper_method :sort_column, :sort_directions
+
   def index
-    @videos = Video.includes(:leader, :follower).order(params[:sort])
-    @leaders = Leader.all
+    @videos    = Video.includes(:leader, :follower).order(sort_column + " " + sort_direction)
+    @leaders   = Leader.all
     @followers = Follower.all
   end
-end
+
 
 private
 
-def sort_column
-  Video.column_names.include?(params[:sort]) ? params[:sort] : "name"
-end
+  def sort_column
+    Video.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
 
-def sort_direction
-  %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 end
