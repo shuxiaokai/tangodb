b5c330911994abceb65fadae8a94a2654493dea7
Justin <jmarsh24@gmail.com>
2020-07-09 15:25:14 +0200

    Filter table bottom fix

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,18 @@
 class VideosController < ApplicationController
 
   def index
-    @videos = Video.all.includes(:leader).limit(1000)
+    @videos = Video.includes(:leader, :follower).order(params[:sort])
+    @leaders = Leader.all
+    @followers = Follower.all
   end
+end
+
+private
+
+def sort_column
+  Video.column_names.include?(params[:sort]) ? params[:sort] : "name"
+end
+
+def sort_direction
+  %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
 end
