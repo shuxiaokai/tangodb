4ee8b3e445da74b057bc0a6b62e930c7ec818df2
Justin <jmarsh24@gmail.com>
2020-07-23 18:24:12 +0200

    fixed sorting by upload_date, view_count, song, artist and moxed search back to main table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,12 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "desc"
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
 
   def current_page_params
-    request.params.slice("youtube_id","sort","direction","leader_id","follower_id","channel_id")
+    request.params.slice("youtube_id","sort","direction","leader_id","follower_id","channel_id","upload_date","view_count","song","arist")
   end
 
   def form_for_object_from_param(param)
