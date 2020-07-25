1d34bb59a7ead4ef13bc9b936e64c5904ebba4aa
Justin <jmarsh24@gmail.com>
2020-07-25 18:48:50 +0200

    Fix filter and main table params

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,17 @@
 class VideosController < ApplicationController
+
   helper_method :sort_column, :sort_direction
+
   include Pagy::Backend
+
   def index
     
     if params[:search_by_keyword]
-             @videos = Video.filter(params.slice(:leader_id, :follower_id, :channel)).includes(:leader, :follower).search_by_keyword(params[:search_by_keyword]).order(sort_column + " " + sort_direction)
-      @pagy, @v_pagination = pagy(Video.filter(params.slice(:leader_id, :follower_id, :channel)).includes(:leader, :follower).search_by_keyword(params[:search_by_keyword]).order(sort_column + " " + sort_direction), items: 100)
+             @videos = Video.filter(params.slice(:leader_id, :follower_id, :channel, :song_id, :view_count, :upload_date)).order(sort_column + " " + sort_direction).includes(:leader, :follower, :song).search_by_keyword(params[:search_by_keyword])
+      @pagy, @v_pagination = pagy(Video.filter(params.slice(:leader_id, :follower_id, :channel, :song_id, :view_count, :upload_date)).order(sort_column + " " + sort_direction).includes(:leader, :follower, :song).search_by_keyword(params[:search_by_keyword]), items: 100)
     else
-             @videos = Video.filter(params.slice(:leader_id, :follower_id, :channel)).includes(:leader, :follower).order(sort_column + " " + sort_direction)
-      @pagy, @v_pagination = pagy(Video.filter(params.slice(:leader_id, :follower_id, :channel)).includes(:leader, :follower).order(sort_column + " " + sort_direction), items: 100)
+             @videos = Video.filter(params.slice(:leader_id, :follower_id, :channel, :song_id, :view_count, :upload_date)).order(sort_column + " " + sort_direction).includes(:leader, :follower, :song)
+      @pagy, @v_pagination = pagy(Video.filter(params.slice(:leader_id, :follower_id, :channel, :song_id, :view_count, :upload_date)).order(sort_column + " " + sort_direction).includes(:leader, :follower, :song), items: 100)
     end
       
   end
@@ -21,13 +24,13 @@ class VideosController < ApplicationController
 
 private
 
-    # A list of the param names that can be used for filtering the Product list
+    # A list of the param names that can be used for filtering the Video list
   def filtering_params(params)
-    params.slice(:with_leader, :with_follower, :with_channel)
+    params.slice(:with_leader, :with_follower, :with_channel, :with_song_id, :with_channel, :with_view_count, :with_upload_date)
   end
 
   def sort_column
-    Video.column_names.include?(params[:sort]) ? params[:sort] : "view_count"
+    Video.column_names.include?(params[:sort]) ? params[:sort] : "upload_date"
   end
 
   def sort_direction
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, current_page_params.merge( {:sort => column, :direction => direction} ), {:class => css_class}
   end
 
   def current_page_params
