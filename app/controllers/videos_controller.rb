86c6bacc86c4b2109f13ccdbb54b9614e5548c79
Justin <jmarsh24@gmail.com>
2020-09-09 19:45:00 +0200

    50 search results

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   # before_action :authenticate_user!
 
-  NUMBER_OF_VIDEOS_PER_PAGE = 10.freeze
+  NUMBER_OF_VIDEOS_PER_PAGE = 50.freeze
   HERO_YOUTUBE_ID = 's6iptZdCcG0'.freeze
 
   helper_method :sort_column, :sort_direction
