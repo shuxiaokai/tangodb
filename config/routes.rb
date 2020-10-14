57cf37f7c846544a8dae30e8149d359f628e3790
Justin <jmarsh24@gmail.com>
2020-10-14 18:54:24 +0200

    stimulus tutorial for filter

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post 'savenew', to: 'users#savenew'
 
+  post 'videos', action: :index, controller: 'videos'
+
   resources :videos, only: %i[index]
 
   namespace :admin do
