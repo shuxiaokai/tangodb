94dee0310b19bfa69fbd0da7e4b95755127e49df
Justin <jmarsh24@gmail.com>
2020-10-17 12:49:27 +0200

    url doesn't display all search filters

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -10,11 +10,14 @@ export default class extends Controller {
 
   filter() {
     const url = `${window.location.pathname}?${this.params}`;
+    console.log(`${window.location.pathname}?${this.params}`);
     Turbolinks.clearCache();
     Turbolinks.visit(url);
   }
 
   get params() {
-    return this.filterTargets.map((t) => `${t.name}=${t.value}`).join("&");
+    // return this.filterTargets.map((t) => `${t.name}=${t.value}`).join("&");
+    return this.filterTargets.filter(t => t.value !== "all").map((t) => `${t.name}=${t.value}`).join("&");
   }
+  
 }
