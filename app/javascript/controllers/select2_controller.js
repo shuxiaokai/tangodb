769c64f8a30996eb726012d9b58e6a64ead58766
Justin <jmarsh24@gmail.com>
2020-10-22 17:53:39 +0200

    git add .

---
+++ app/javascript/controllers/select2_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus"
+import $ from 'jquery';
+
+require("select2/dist/css/select2")
+
+import Select2 from "select2"
+
+export default class extends Controller {
+  connect() {
+    console.log('Select2 Controller ON')
+    var list = $('.content-search').select2({
+      closeOnSelect: false,
+      }).on("select2:closing", function(e) {
+        e.preventDefault();
+      }).on("select2:closed", function(e) {
+        list.select2("open");
+      }).on("select2:select", function() {
+        let event = new Event('change', { bubbles: true })
+        this.dispatchEvent(event)
+      }).select2("open");
+    }
+  }
