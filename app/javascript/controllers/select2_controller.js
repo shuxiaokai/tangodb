3e917b0dbe9f48e1739f89362aaaae1ef0dfbaa7
Justin <jmarsh24@gmail.com>
2020-10-22 19:50:41 +0200

    added to select2.js https://jsfiddle.net/g4maj9ox/

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -10,6 +10,7 @@ export default class extends Controller {
     console.log('Select2 Controller ON')
     var list = $('.content-search').select2({
       closeOnSelect: false,
+      dropdownPosition: 'below',
       }).on("select2:closing", function(e) {
         e.preventDefault();
       }).on("select2:closed", function(e) {
@@ -17,6 +18,26 @@ export default class extends Controller {
       }).on("select2:select", function() {
         let event = new Event('change', { bubbles: true })
         this.dispatchEvent(event)
-      }).select2("open");
+      }).on('select2-open', function() {
+
+        // however much room you determine you need to prevent jumping
+        var requireHeight = 600;
+        var viewportBottom = $(window).scrollTop() + $(window).height();
+
+        // figure out if we need to make changes
+        if (viewportBottom < requireHeight) 
+        {           
+            // determine how much padding we should add (via marginBottom)
+            var marginBottom = requireHeight - viewportBottom;
+
+            // adding padding so we can scroll down
+            $(".aLwrElmntOrCntntWrppr").css("marginBottom", marginBottom + "px");
+
+            // animate to just above the select2, now with plenty of room below
+            $('html, body').animate({
+                scrollTop: $("#mySelect2").offset().top - 10
+            }, 1000);
+        }
+    }).select2("open");
     }
   }
