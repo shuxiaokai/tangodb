bcf778de7b4d42b72404d6c37eaad4d33be95912
Justin <jmarsh24@gmail.com>
2020-10-23 14:05:27 +0200

    search back to url

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,6 +1,6 @@
 // filters_controller.js
 import { Controller } from "stimulus";
-import Rails from "@rails/ujs";
+// import Rails from "@rails/ujs";
 
 export default class extends Controller {
   static targets = ["filter"];
@@ -10,22 +10,29 @@ export default class extends Controller {
   }
 
   filter() {
+    console.log(this.params)
     const url = `${window.location.pathname}?${this.params}`;
-
-    Rails.ajax({
-     type: "get",
-     url: url,
-     success: (data) => {
-       const newContainer = data.getElementById("videos");
-       const container = document.getElementById("videos");
-
-       container.innerHTML = newContainer.innerHTML;
-     },
-     error: (data) => {
-       console.log(data)
-     }
-   });
+    Turbolinks.clearCache();
+    Turbolinks.visit(url);
   }
+
+  // filter() {
+  //   const url = `${window.location.pathname}?${this.params}`;
+
+  //   Rails.ajax({
+  //    type: "get",
+  //    url: url,
+  //    success: (data) => {
+  //      const newContainer = data.getElementById("videos");
+  //      const container = document.getElementById("videos");
+
+  //      container.innerHTML = newContainer.innerHTML;
+  //    },
+  //    error: (data) => {
+  //      console.log(data)
+  //    }
+  //  });
+  // }
 
   get params() {
     // return this.filterTargets.map((t) => `${t.name}=${t.value}`).join("&");
