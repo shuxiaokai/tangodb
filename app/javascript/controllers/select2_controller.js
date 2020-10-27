002e1664a8820870c08b4142a56758ffab120760
Justin <jmarsh24@gmail.com>
2020-10-27 09:54:50 +0100

    WIP filter partial

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -21,6 +21,26 @@ export default class extends Controller {
       }).on("select2:select", function() {
         let event = new Event('change', { bubbles: true })
         this.dispatchEvent(event)
+      }).on('select2-open', function() {
+
+        // however much room you determine you need to prevent jumping
+        var requireHeight = 600;
+        var viewportBottom = $(window).scrollTop() + $(window).height();
+
+        // figure out if we need to make changes
+        if (viewportBottom < requireHeight) 
+        {           
+          // determine how much padding we should add (via marginBottom)
+          var marginBottom = requireHeight - viewportBottom;
+
+          // adding padding so we can scroll down
+          $(".aLwrElmntOrCntntWrppr").css("marginBottom", marginBottom + "px");
+
+          // animate to just above the select2, now with plenty of room below
+          $('html, body').animate({
+              scrollTop: $("#mySelect2").offset().top - 10
+          }, 1000);
+        }
       }).select2("open");
     }
   }
