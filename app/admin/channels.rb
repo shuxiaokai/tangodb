66e4724a3a5e17798c1f984c6ab7ff2f0f741471
Jmarsh24 <jmarsh24@gmail.com>
2020-12-21 12:37:32 +0100

    add channels to active admin

---
+++ app/admin/channels.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Channel do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :channel_id, :thumbnail_url, :imported, :imported_videos_count, :total_videos_count, :yt_api_pull_count
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :channel_id, :thumbnail_url, :imported, :imported_videos_count, :total_videos_count, :yt_api_pull_count]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
