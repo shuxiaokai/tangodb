6c7d6459195ab588d953624462e02bf3dfcb69f8
Jmarsh24 <jmarsh24@gmail.com>
2020-12-23 14:20:21 +0100

    add jquery to activeadmin js file

--- app/javascript/packs/active_admin.js
+++ app/javascript/packs/active_admin.js
@@ -1,5 +1,9 @@
 // Load Active Admin's styles into Webpacker,
 // see `active_admin.scss` for customization.
+
+//= require jquery
+//= require jquery_ujs
+
 import "../stylesheets/active_admin";
 
 import "@activeadmin/activeadmin";
