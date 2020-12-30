969ecd987a267957086e8f76ea286d2f5479149d
Jmarsh24 <jmarsh24@gmail.com>
2020-12-30 21:18:31 +0100

    depedent destroy for channels

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -18,7 +18,7 @@ class Channel < ApplicationRecord
   validates :channel_id, presence: true, uniqueness: true
   validates :title, presence: true, uniqueness: true
 
-  has_many :videos
+  has_many :videos, dependent: :destroy
 
   scope :imported,     ->   { where(imported: true) }
   scope :not_imported, ->   { where(imported: false) }
