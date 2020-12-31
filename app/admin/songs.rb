268dcf7cc791db8a701a7ab379797a85a5ef1dd1
Jmarsh24 <jmarsh24@gmail.com>
2020-12-31 17:08:43 +0100

    install activeadmin addons

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -1,6 +1,5 @@
 ActiveAdmin.register Song do
-
-  permit_params :genre, :title, :artist
+  permit_params :genre, :title, :artist, :artist_2, :active, :last_name_search, :date
 
   config.sort_order = 'id_asc'
 
@@ -11,6 +10,9 @@ ActiveAdmin.register Song do
     column :title
     column :artist
     column :artist_2
+    column :last_name_search
+    toggle_bool_column :active
+    column :date
     actions
   end
 end
