aa1649f71e5c82c4c42ffd22b753ddd834326e14
Jmarsh24 <jmarsh24@gmail.com>
2021-01-07 20:23:41 +0100

    working pg_search

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,11 +6,10 @@ class VideosController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @videos = Video.includes(:leader, :follower, :channel, :song)
+    @videos = Video.includes(:leader, :follower, :song, :channel)
                    .filter_videos(params.slice(:leader, :follower, :channel, :genre, :query))
                    .where.not('leader_id IS NULL AND follower_id IS NULL AND song_id IS NULL ')
                    .where(hidden: false)
-                   .references(:leader, :follower, :channel, :song)
                    .order(sort_column + ' ' + sort_direction)
 
     @videos_paginated = @videos.paginate(page, NUMBER_OF_VIDEOS_PER_PAGE)
@@ -20,10 +19,15 @@ class VideosController < ApplicationController
     @channel_count  = Video.pluck(:channel_id).compact.uniq.count
     @genre_count    = Video.joins(:song).pluck(:genre).compact.uniq.count
 
-    @leaders    = @videos.joins(:leader).pluck(:"leaders.name").compact.uniq.map(&:titleize).sort
-    @followers  = @videos.joins(:follower).pluck(:"followers.name").compact.uniq.map(&:titleize).sort
-    @channels   = @videos.joins(:channel).pluck(:"channels.title").compact.uniq.sort
-    @genres     = @videos.joins(:song).pluck(:"songs.genre").compact.uniq.map(&:capitalize).sort
+    leader_name   = params[:query].present? ? 'leaders_videos.name'   : 'leaders.name'
+    follower_name = params[:query].present? ? 'followers_videos.name' : 'followers.name'
+    channel_title = params[:query].present? ? 'channels_videos.title' : 'channels.title'
+    songs_genre   = params[:query].present? ? 'songs_videos.genre'    : 'songs.genre'
+
+    @leaders    = @videos.joins(:leader).pluck(leader_name).compact.uniq.sort.map(&:titleize)
+    @followers  = @videos.joins(:follower).pluck(follower_name).compact.uniq.sort.map(&:titleize)
+    @channels   = @videos.joins(:channel).pluck(channel_title).compact.uniq.sort
+    @genres     = @videos.joins(:song).pluck(songs_genre).compact.uniq.sort.map(&:titleize)
   end
 
   private
