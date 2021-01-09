5b68111dea18e873dc6f481f269663fe15e15511
Jmarsh24 <jmarsh24@gmail.com>
2021-01-10 00:20:50 +0100

    only show songs with result

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,17 +1,17 @@
 class SearchSuggestionsController < ApplicationController
   def index
-    @leaders = Leader.where('unaccent(name) ILIKE ?', "%#{params[:q]}%").limit(5).pluck(:name)
-    @followers = Follower.where('unaccent(name) ILIKE ?', "%#{params[:q]}%").limit(5).pluck(:name)
-    @songs = Song.where('unaccent(artist) ILIKE ?
-                        OR unaccent(title) ILIKE ?
-                        OR unaccent(genre)ILIKE ?',
+    @leaders = Leader.where('unaccent(name) ILIKE unaccent(?)', "%#{params[:q]}_%").limit(10).pluck(:name)
+    @followers = Follower.where('unaccent(name) ILIKE unaccent(?)', "%#{params[:q]}_%").limit(10).pluck(:name)
+    @songs = Song.joins(:videos).distinct
+                 .where('unaccent(songs.artist) ILIKE unaccent(?)
+                        OR unaccent(songs.title) ILIKE unaccent(?)
+                        OR unaccent(songs.genre) ILIKE unaccent(?)',
                         "%#{params[:q]}%",
                         "%#{params[:q]}%",
                         "%#{params[:q]}%")
-                 .limit(5)
-                 .distinct
-                 .pluck("title || ' | ' || artist || ' | ' || genre")
-    @suggestions = [@leaders + @followers + @songs].flatten.first(5).map(&:titleize)
+                 .limit(10)
+                 .pluck("songs.title || ' | ' || songs.artist || ' | ' || songs.genre")
+    @suggestions = [@leaders + @followers + @songs].flatten.first(10).map(&:titleize)
     render layout: false
   end
 end
