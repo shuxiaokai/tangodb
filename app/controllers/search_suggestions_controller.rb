0e06d92a2ee60afadda2aafe45e143c9bc214bfb
Jmarsh24 <jmarsh24@gmail.com>
2021-01-09 18:54:52 +0100

    change order for sorting to increase search results

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -2,7 +2,15 @@ class SearchSuggestionsController < ApplicationController
   def index
     @leaders = Leader.where('unaccent(name) ILIKE ?', "%#{params[:q]}%").limit(5).pluck(:name)
     @followers = Follower.where('unaccent(name) ILIKE ?', "%#{params[:q]}%").limit(5).pluck(:name)
-    @songs = Song.where('unaccent(artist) ILIKE ? OR unaccent(title) ILIKE ? OR unaccent(genre)ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%").limit(5).distinct.pluck("title || ' | ' || artist || ' | ' || genre")
+    @songs = Song.where('unaccent(artist) ILIKE ?
+                        OR unaccent(title) ILIKE ?
+                        OR unaccent(genre)ILIKE ?',
+                        "%#{params[:q]}%",
+                        "%#{params[:q]}%",
+                        "%#{params[:q]}%")
+                 .limit(5)
+                 .distinct
+                 .pluck("title || ' | ' || artist || ' | ' || genre")
     @suggestions = [@leaders + @followers + @songs].flatten.first(5).map(&:titleize)
     render layout: false
   end
