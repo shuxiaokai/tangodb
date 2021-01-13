8df9faba0588e525b1d25bb9683190f40d6945cd
Jmarsh24 <jmarsh24@gmail.com>
2021-01-13 15:13:14 +0100

    new layout with tsvector index

---
+++ db/migrate/20210112185333_add_index_to_column_to_videos.rb
@@ -0,0 +1,7 @@
+class AddIndexToColumnToVideos < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
+  def change
+    add_index :videos, :searchable, using: :gin, algorithm: :concurrently
+  end
+end
