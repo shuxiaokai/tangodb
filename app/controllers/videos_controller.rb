b8f53f7724f1a234b23550d88bc7ea3082a187dc
Jmarsh24 <jmarsh24@gmail.com>
2021-01-13 18:46:08 +0100

    lower qualtity thumbnails

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   # before_action :authenticate_user!
 
-  NUMBER_OF_VIDEOS_PER_PAGE = 20
+  NUMBER_OF_VIDEOS_PER_PAGE = 30
 
   helper_method :sort_column, :sort_direction
 
