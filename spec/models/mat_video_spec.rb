3bed302641f7f101a93ba8af6cc210c094add200
Jmarsh24 <jmarsh24@gmail.com>
2021-01-16 16:40:56 +0100

    add commented out test for mat_video

--- spec/models/mat_video_spec.rb
+++ spec/models/mat_video_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe MatVideo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe MatTopScorer, type: :model do
+#   describe "#top_scorer_for_season" do
+#     it "finds top scorer" do
+#       # create some data using factory_bot helper methods
+#       match = create(:match)
+#       player = create(:player)
+#       goal = create(:goal, match: match, player: player)
+
+#       # without any data in materialized view
+#       expect(MatTopScorer.top_scorer_for_season(match.season)).to eq(nil)
+
+#       MatTopScorer.refresh
+
+#       # with data in materialized view
+#       top_scorer = MatTopScorer.top_scorer_for_season(match.season)
+#       expect(top_scorer).to be_present
+#       expect(top_scorer.player).to eq(player)
+#       expect(top_scorer.goal_count).to eq(1)
+#     end
+#   end
+# end
