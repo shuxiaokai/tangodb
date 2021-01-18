a670d773f79521bfb1d96cb1991ce57a9be92f96
Jmarsh24 <jmarsh24@gmail.com>
2021-01-18 13:47:48 +0100

    revamp materialized view

---
+++ app/models/videos_search.rb
@@ -0,0 +1,24 @@
+class VideosSearch < ApplicationRecord
+  self.primary_key = :video_id
+
+  include PgSearch::Model
+
+  pg_search_scope( :search, against: :tsv_document,
+                    using: {
+                      tsearch: {
+                        dictionary: 'english',
+                        tsvector_column: 'tsv_document',
+                        prefix: true
+                      },
+                    },
+                    ignoring: :accents
+                  )
+
+  def self.refresh
+    Scenic.database.refresh_materialized_view(:videos_searches, concurrently: false, cascade: false)
+  end
+
+  def readonly?
+    true
+  end
+end
---
+++ db/views/videos_searches_v01.sql
@@ -0,0 +1,24 @@
+SELECT
+  videos.id AS video_id,
+  (    to_tsvector('english', coalesce(videos.title, ''))
+    || to_tsvector('english', coalesce(videos.description, ''))
+    || to_tsvector('english', coalesce(videos.youtube_id, ''))
+    || to_tsvector('english', coalesce(videos.youtube_artist, ''))
+    || to_tsvector('english', coalesce(videos.youtube_song, ''))
+    || to_tsvector('english', coalesce(videos.spotify_track_name, ''))
+    || to_tsvector('english', coalesce(videos.spotify_artist_name, ''))
+    || to_tsvector('english', coalesce(channels.title, ''))
+    || to_tsvector('english', coalesce(channels.channel_id, ''))
+    || to_tsvector('english', coalesce(leaders.name, ''))
+    || to_tsvector('english', coalesce(leaders.nickname, ''))
+    || to_tsvector('english', coalesce(followers.name, ''))
+    || to_tsvector('english', coalesce(followers.nickname, ''))
+    || to_tsvector('english', coalesce(songs.genre, ''))
+    || to_tsvector('english', coalesce(songs.title, ''))
+    || to_tsvector('english', coalesce(songs.artist, ''))
+  ) AS tsv_document
+FROM videos
+LEFT OUTER JOIN  channels ON channels.id = videos.channel_id
+LEFT OUTER JOIN  followers ON followers.id = videos.follower_id
+LEFT OUTER JOIN  leaders ON leaders.id = videos.leader_id
+LEFT OUTER JOIN  songs ON songs.id = videos.song_id;
