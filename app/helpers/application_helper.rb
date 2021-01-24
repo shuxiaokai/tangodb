f2900e69b92d089360fafaf80896698f23f1d9e8
Jmarsh24 <jmarsh24@gmail.com>
2021-01-24 17:24:23 +0100

    fix sorting default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'desc'
+    direction = column == sort_column && sort_direction == 'desc' ? 'asc' : 'desc'
     link_to "#{title} #{if css_class.present?
                           content_tag(:i, '', class: "fa fa-chevron-#{direction == 'asc' ? 'up' : 'down'}")
                         end}".html_safe,
