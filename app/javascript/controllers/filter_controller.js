da1eed1ad9d9b06c165f87ff15dc5a15017a49eb
Jmarsh24 <jmarsh24@gmail.com>
2021-01-26 12:42:53 +0100

    turbo incorporated with filter controller

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,6 +1,7 @@
 // filters_controller.js
 import { Controller } from "stimulus";
-import Rails from "@rails/ujs";
+// import Rails from "@rails/ujs";
+import * as Turbo from '@hotwired/turbo';
 
 export default class extends Controller {
   static targets = ["filter"];
@@ -9,33 +10,33 @@ export default class extends Controller {
     console.log("Filter-Controller ON");
   }
 
-  // filter() {
-  //   console.log(window.location.pathname);
-  //   const url = `${window.location.pathname}?${this.params}`;
-  //   Turbo.clearCache();
-  //   Turbo.visit(url);
-  // }
-
   filter() {
+    console.log(window.location.pathname);
     const url = `${window.location.pathname}?${this.params}`;
-
-    Rails.ajax({
-     type: "get",
-     url: url,
-     success: (data) => {
-       const newContainerVideos = data.getElementById("videos");
-       const containerVideos = document.getElementById("videos");
-       const containerFilters = document.getElementById("filters");
-       const newContainerFilters = data.getElementById("filters");
-       containerVideos.innerHTML = newContainerVideos.innerHTML;
-       containerFilters.innerHTML = newContainerFilters.innerHTML;
-       history.pushState({}, '', `${window.location.pathname}?${this.params}`)
-     },
-     error: (data) => {
-       console.log(data)
-     }
-   });
+    Turbo.clearCache();
+    Turbo.visit(url);
   }
+
+  // filter() {
+  //   const url = `${window.location.pathname}?${this.params}`;
+
+  //   Rails.ajax({
+  //    type: "get",
+  //    url: url,
+  //    success: (data) => {
+  //      const newContainerVideos = data.getElementById("videos");
+  //      const containerVideos = document.getElementById("videos");
+  //      const containerFilters = document.getElementById("filters");
+  //      const newContainerFilters = data.getElementById("filters");
+  //      containerVideos.innerHTML = newContainerVideos.innerHTML;
+  //      containerFilters.innerHTML = newContainerFilters.innerHTML;
+  //      history.pushState({}, '', `${window.location.pathname}?${this.params}`)
+  //    },
+  //    error: (data) => {
+  //      console.log(data)
+  //    }
+  //  });
+  // }
 
   get params() {
     let params = this.filterTargets
