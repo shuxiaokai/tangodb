3d06aa6d76826d3a368de76763d887ce24a8dd21
Jmarsh24 <jmarsh24@gmail.com>
2021-01-26 19:24:25 +0100

    fix sorting on playlist imported videos

--- app/controllers/watch_controller.rb
+++ app/controllers/watch_controller.rb
@@ -1,5 +1,6 @@
 class WatchController < ApplicationController
   def show
     @active_video = Video.find_by(youtube_id: params[:v])
+    @active_video.update(popularity: @active_video.popularity + 1)
   end
 end
