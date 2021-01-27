c9bb5fd833627ba24bb6c9719befde485302f1ca
Jmarsh24 <jmarsh24@gmail.com>
2021-01-28 00:01:46 +0100

    remove padding from search suggestions

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,22 +1,19 @@
 class SearchSuggestionsController < ApplicationController
   def index
-
-
-
     @leaders   = Leader.joins(:videos).distinct.where('unaccent(name) ILIKE unaccent(?)',
                                                       "%#{params[:q]}%").limit(10).pluck(:name)
 
     @followers = Follower.joins(:videos).distinct.where('unaccent(name) ILIKE unaccent(?)',
                                                         "%#{params[:q]}%").limit(10).pluck(:name)
-        @songs = Song.joins(:videos)
-                     .search(params[:q])
-                     .reorder('')
-                     .distinct
-                     .limit(10)
-                     .pluck('songs.title', 'songs.artist', 'songs.genre').map { |songs| songs.join(" ") }
+    @songs = Song.joins(:videos)
+                 .search(params[:q])
+                 .reorder('')
+                 .distinct
+                 .limit(10)
+                 .pluck('songs.title', 'songs.artist', 'songs.genre').map { |songs| songs.join(' ') }
 
-      @channels = Channel.where('unaccent(title) ILIKE unaccent(?)',
-                                                        "%#{params[:q]}%").limit(10).pluck(:title)
+    @channels = Channel.where('unaccent(title) ILIKE unaccent(?)',
+                              "%#{params[:q]}%").limit(10).pluck(:title)
 
     @suggestions = [@leaders + @followers + @songs + @channels].flatten.first(10).map(&:titleize)
     render layout: false
