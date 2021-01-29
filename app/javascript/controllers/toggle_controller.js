38569a593305cc892dd0212a7335b9b67d8caf61
Jmarsh24 <jmarsh24@gmail.com>
2021-01-30 00:58:40 +0100

    completely revamp css front end

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,15 +1,20 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-static targets = ["toggleable"];
+static targets = ["toggleable", "sideNavContainer", "mainSectionContainer"];
 
   connect() {
     console.log("Toggle-Controller ON");
   }
 
-
   toggle() {
     console.log("clicked");
-    this.toggleableTarget.classList.toggle("is-hidden");
+    this.toggleableTarget.classList.toggle("isHidden");
+  }
+
+  navShowHide() {
+      console.log('clicked')
+      this.sideNavContainerTarget.classList.toggle('isHidden')
+      this.mainSectionContainerTarget.classList.toggle('leftPadding')
   }
 }
