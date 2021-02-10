0a3bc3abf04d107cc1a643bf21542c0ef0311edb
Jmarsh24 <jmarsh24@gmail.com>
2021-02-10 23:36:08 +0100

    fix the pagination gaps

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   # before_action :authenticate_user!
 
-  NUMBER_OF_VIDEOS_PER_PAGE = 32
+  NUMBER_OF_VIDEOS_PER_PAGE = 60
 
   helper_method :sort_column, :sort_direction
 
