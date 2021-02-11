0fb59d922f396aa5a468084b6a4a0e18da7db358
Jmarsh24 <jmarsh24@gmail.com>
2021-02-11 14:14:05 +0100

    autoplay modest branding an rel 0

--- app/models/video.rb
+++ app/models/video.rb
@@ -159,6 +159,11 @@ class Video < ApplicationRecord
 
     def match_dancer(dancer)
       keywords = {}
+      partners = {}
+
+      partners.merge if dancer.follower.present?
+
+      partner.merge!(partner_last_name: '%last_name%') if dancer.leader.present?
 
       if dancer.first_name.present? && dancer.last_name.present?
         keywords.merge!(name_1: "%#{dancer.first_name[0, 1]}.#{dancer.last_name}%")
