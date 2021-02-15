3406bb1e8f6f844a50df1cf9e5d65842ea7d58cf
Jmarsh24 <jmarsh24@gmail.com>
2021-02-15 18:10:57 +0100

    add pagination 120

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   # before_action :authenticate_user!
 
-  NUMBER_OF_VIDEOS_PER_PAGE = 60
+  NUMBER_OF_VIDEOS_PER_PAGE = 120
 
   helper_method :sort_column, :sort_direction
 
