341da6d7e2acd4b754a4557a74e8b15dfc15e1f7
Jmarsh24 <jmarsh24@gmail.com>
2021-02-23 19:33:56 +0100

    restore back

--- app/javascript/controllers/slimselect_controller.js
+++ app/javascript/controllers/slimselect_controller.js
@@ -1,50 +1,13 @@
 import { Controller } from 'stimulus'
 import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.min.css'
+import '..//stylesheets/slimselect-customized.scss'
 
 export default class extends Controller {
    static targets = ['field']
 
   connect() {
     const url = this.fieldTarget.dataset.slimselectUrl
-
-    // new SlimSelect({
-    //   select: this.element,
-    //   searchHighlight: true,
-    //   closeOnSelect: false,
-    //   showContent: 'down',
-    //   searchingText: 'Searching...', // Optional - Will show during ajax request
-    //   ajax: function (search, callback) {
-    //     // Check search value. If you dont like it callback(false) or callback('Message String')
-    //     if (search.length < 3) {
-    //       callback('Need 3 characters')
-    //       return
-    //     }
-
-    //     // Perform your own ajax request here
-    //     fetch(url + '?q=' + search)
-    //     .then(function (response) {
-    //       return response.json()
-    //     })
-    //     .then(function (json) {
-    //       let data = []
-    //       for (let i = 0; i < json.length; i++) {
-    //         data.push({ value: json[i][1], text: json[i][0] })
-    //       }
-
-    //       // Upon successful fetch send data to callback function.
-    //       // Be sure to send data back in the proper format.
-    //       // Refer to the method setData for examples of proper format.
-    //       console.log(data)
-    //       callback(data)
-    //     })
-    //     .catch(function(error) {
-    //       // If any erros happened send false back through the callback
-    //       callback(false)
-    //     })
-    //   }
-    // })
-
     const limit = this.data.get('limit')
     const placeholder = this.data.get('placeholder')
     const searchText = this.data.get('no-results')
@@ -55,7 +18,7 @@ export default class extends Controller {
 
     var select = new SlimSelect({
         select: this.element,
-        beforeClose: function (event) { event.preventDefault().select.open()},
+        beforeClose: function (event) { event.slim.open()},
         closeOnSelect,
         allowDeselect,
         limit,
@@ -65,11 +28,41 @@ export default class extends Controller {
         searchFocus
       })
       select.open()
-  }
+  // new SlimSelect({
+  //   select: this.element,
+  //   searchHighlight: true,
+  //   closeOnSelect: false,
+  //   showContent: 'down',
+  //   searchingText: 'Searching...', // Optional - Will show during ajax request
+  //   ajax: function (search, callback) {
+  //     // Check search value. If you dont like it callback(false) or callback('Message String')
+  //     if (search.length < 3) {
+  //       callback('Need 3 characters')
+  //       return
+  //     }
 
-  open() {
-    console.log('slimselect open fired')
-    var select = document.querySelectorAll('select')
-    select.forEach(select => select.slim.open())
+  //     // Perform your own ajax request here
+  //     fetch(url + '?q=' + search)
+  //     .then(function (response) {
+  //       return response.json()
+  //     })
+  //     .then(function (json) {
+  //       let data = []
+  //       for (let i = 0; i < json.length; i++) {
+  //         data.push({ value: json[i][1], text: json[i][0] })
+  //       }
+
+  //       // Upon successful fetch send data to callback function.
+  //       // Be sure to send data back in the proper format.
+  //       // Refer to the method setData for examples of proper format.
+  //       console.log(data)
+  //       callback(data)
+  //     })
+  //     .catch(function(error) {
+  //       // If any erros happened send false back through the callback
+  //       callback(false)
+  //     })
+  //   }
+  // })
   }
 }
