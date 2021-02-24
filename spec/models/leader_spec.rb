7c52d67c048a5bec726b4078d951d64df7b4d80b
Jmarsh24 <jmarsh24@gmail.com>
2021-02-24 21:59:56 +0100

    move simple cov to spec helper

--- spec/models/leader_spec.rb
+++ spec/models/leader_spec.rb
@@ -14,16 +14,37 @@
 require 'rails_helper'
 
 RSpec.describe Leader, type: :model do
-  it { is_expected.to have_many(:videos) }
-
   let(:leader) { build(:leader) }
 
   context 'validation tests' do
+    subject { FactoryBot.build(:leader) }
+    it { is_expected.to have_many(:videos) }
+    it { is_expected.to have_many(:follower) }
+    it { is_expected.to have_many(:song) }
+    it { should validate_uniqueness_of(:name) }
+
     it 'ensures name presence' do
       leader.name = nil
       expect(leader.save).to eq(false)
     end
   end
+
   context 'scope tests' do
+    it 'includes leaders with reviewed flagged' do
+      leader = create(:leader, reviewed: true)
+      expect(Leader.reviewed).to include(leader)
+    end
+
+    it 'includes leaders without reviewed flagged' do
+      leader = create(:leader, reviewed: false)
+      expect(Leader.not_reviewed).to include(leader)
+    end
+  end
+
+  context 'method tests' do
+    it 'tests full_name to return "first_name last_name"' do
+      leader = create(:leader)
+      expect(leader.full_name).to eq(leader.first_name + ' ' + leader.last_name)
+    end
   end
 end
