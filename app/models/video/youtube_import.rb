4c41abb10fefa74a045c28ed8efa645a3486731c
Jmarsh24 <jmarsh24@gmail.com>
2021-02-28 22:10:09 +0100

    add import videos to youtube_import

--- app/models/video/youtube_import.rb
+++ app/models/video/youtube_import.rb
@@ -2,10 +2,12 @@ class Video::YoutubeImport
   class << self
     def from_channel(channel_id)
       Channel.import(channel_id)
+      Channel.import_videos(channel_id)
     end
 
     def from_playlist(playlist_id)
       Playlist.import(playlist_id)
+      Playlist.import_videos(playlist_id)
     end
 
     def from_video(video_id)
