1ee0787bc1b395d54269b132892b7b113d60acd2
Jmarsh24 <jmarsh24@gmail.com>
2021-03-06 20:26:03 +0100

    fix depreciation warning on rpsec

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -13,30 +13,10 @@
 #
 
 class Follower < ApplicationRecord
+  include FullNameable
   validates :name, presence: true, uniqueness: true
 
   has_many :videos
   has_many :leader, through: :videos
   has_many :song, through: :videos
-
-  scope :reviewed,     ->   { where(reviewed: true) }
-  scope :not_reviewed, ->   { where(reviewed: false) }
-  scope :full_name_search, lambda { |query|
-                             where("unaccent(name) ILIKE unaccent(:query) OR
-                                    unaccent(first_name) ILIKE unaccent(:query) OR
-                                    unaccent(last_name) ILIKE unaccent(:query)",
-                                   query: "%#{query}%")
-                           }
-
-  def full_name
-    first_name.present? && last_name.present? ? "#{first_name} #{last_name}" : name
-  end
-
-  def abrev_name
-    first_name.present? && last_name.present? ? "#{first_name.first}. #{last_name}" : nil
-  end
-
-  def abrev_name_nospace
-    first_name.present? && last_name.present? ? "#{first_name.first}.#{last_name}" : nil
-  end
 end
