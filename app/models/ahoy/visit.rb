0caca1d66107fb57f9d38a544bd19ab22176e14c
Jmarsh24 <jmarsh24@gmail.com>
2021-03-06 19:34:43 +0100

    remove ahoy visit

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -30,8 +30,6 @@
 #  started_at       :datetime
 #
 class Ahoy::Visit < AhoyRecord
-  self.primary_key = "id"
-
   has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
 end
