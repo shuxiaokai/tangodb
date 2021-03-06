2e1df1be35100abd23b662b0c7045b379dbbed08
Jmarsh24 <jmarsh24@gmail.com>
2021-03-07 00:39:07 +0100

    split up song calc

--- app/models/song.rb
+++ app/models/song.rb
@@ -31,6 +31,7 @@ class Song < ApplicationRecord
   scope :filter_by_popularity, -> { where.not(popularity: [false, nil]) }
   scope :filter_by_active,     -> { where(active: true) }
   scope :filter_by_not_active, -> { where(active: false) }
+
   # song match scopes
   scope :title_match, ->(model_attribute) { where("unaccent(title) ILIKE unaccent(?)", "%#{model_attribute}%") }
 
@@ -46,19 +47,34 @@ class Song < ApplicationRecord
   end
 
   class << self
-    # FIXME; way too many lines in this method. What does it do, a method should do one or two manipulation max!
-    # Break that down in smaller parts.
-    def calc_song_popularity
-      occur_num = Video.group(:song_id).count
-      max_value = occur_num.values.max
-      popularity = occur_num.transform_values { |v| (v.to_f / max_value * 100).round }
+    # groups videos by song id and calculates the occur_count of each song
+    def occur_count_all
+      Video.group(:song_id).count.without(nil)
+    end
 
-      occur_num.each do |key, value|
+    # calculates the maximum amount of occurances
+    def max_occur_count_all
+      @max_occur_count_all = occur_count_all.values.max
+    end
+
+    # creates a normalized popularity value between 0-100 based upon the occurance count
+    # and max number of occurances of each song
+    def calc_popularity_all
+      max_occur_count_all
+      occur_count_all.transform_values { |v| (v.to_f / @max_occur_count_all * 100).round }
+    end
+
+    # updates each song in the database with an occurance count
+    def update_all_occur_count
+      occur_count_all.each do |key, value|
         song = Song.find(key)
         song.update(occur_count: value)
       end
+    end
 
-      popularity.each do |key, value|
+    # updates all songs in the database with a popularity number
+    def update_all_popularity
+      occur_count_all.each do |key, value|
         song = Song.find(key)
         song.update(popularity: value)
       end
