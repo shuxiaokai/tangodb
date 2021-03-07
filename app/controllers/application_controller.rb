6a58036e945f33ad4f6f1c1ef1d0072736af9622
Jmarsh24 <jmarsh24@gmail.com>
2021-03-07 21:18:50 +0100

    add private back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   after_action :track_action
 
+  private
+
   def track_action
     ahoy.track "Ran action", request.params
   end
