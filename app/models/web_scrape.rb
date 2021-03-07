be46b4e97eb83498e433dababcf37f3bf4d7a186
Jmarsh24 <jmarsh24@gmail.com>
2021-03-07 23:11:06 +0100

    fix end clause on webscrape

--- app/models/web_scrape.rb
+++ app/models/web_scrape.rb
@@ -1,34 +1,7 @@
 class WebScrape < ApplicationRecord
   class << self
-    def recodo_lyrics
-      (1..20_000).each do |id|
-        logger.info "Page Number: #{id}"
-
-        response = Faraday.get("https://www.el-recodo.com/music?id=#{id}&lang=en")
-
-        doc = Nokogiri::HTML(response.body)
-
-        lyrics = doc.css("p#geniusText").text if doc.css("p#geniusText").present?
-
-        date = doc.css("div.list-group.lead a")[0].text.strip.split[1] if doc.css("div.list-group.lead a")[0].present?
-
-        if doc.css("div.list-group.lead a")[1].present?
-          title = doc.css("div.list-group.lead a")[1].text.strip.split[1..].join(" ")
-        end
-
-        if doc.css("div.list-group.lead a")[3].present?
-          artist = doc.css("div.list-group.lead a")[3].text.strip.split[1..].join(" ")
-        end
-
-        if lyrics.present?
-          song = Song.where("title ILIKE ? AND artist ILIKE ?", title, artist)
-          song.update(el_recodo_song_id: id, lyrics: lyrics)
-        end
-      end
-    end
-
     def tangopolix_events
-      (14..469).each do |id|
+      (1..469).each do |id|
         puts "Page Number: #{id}"
         response = Faraday.get("https://www.tangopolix.com/archived-events/page-#{id}")
         doc = Nokogiri::HTML(response.body)
@@ -49,6 +22,7 @@ class WebScrape < ApplicationRecord
                        start_date: start_date,
                        end_date:   end_date,
                        category:   category)
+        end
       end
     end
   end
