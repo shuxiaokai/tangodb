9254132d76562071c3f449b3f6fe8c4c6d24e3a0
Jmarsh24 <jmarsh24@gmail.com>
2021-03-07 17:07:02 +0100

    move tangopolix event scraper to a new class

--- app/models/event.rb
+++ app/models/event.rb
@@ -27,32 +27,6 @@ class Event < ApplicationRecord
                        }
 
   class << self
-    def scrape_events
-      (14..469).each do |id|
-        puts "Page Number: #{id}"
-        response = Faraday.get("https://www.tangopolix.com/archived-events/page-#{id}")
-        doc = Nokogiri::HTML(response.body)
-        articles = doc.css("article")
-        articles.each do |article|
-          title = article.css("h1 a").text.strip
-          location = article.css("span.uk-icon-medium").text.split(",").map(&:strip)
-          city = location[0]
-          country = location[1]
-          date = article.css("span.uk-icon-calendar").text.split("-").map(&:strip)
-          start_date = date[0]
-          end_date = date[1]
-          category = article.css("div.uk-vertical-align-bottom a")[0].text
-
-          Event.create(title:      title,
-                       city:       city,
-                       country:    country,
-                       start_date: start_date,
-                       end_date:   end_date,
-                       category:   category)
-        end
-      end
-    end
-
     def match_all_events
       Event.all.order(:id).each do |event|
         MatchEventWorker.perform_async(event.id)
