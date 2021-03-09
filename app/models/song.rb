c8eb360652050b7d2d52aff38f833bd57a142676
Jmarsh24 <jmarsh24@gmail.com>
2021-03-09 20:41:49 +0100

    add counter cash for songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -24,7 +24,7 @@ class Song < ApplicationRecord
   validates :title, presence: true
   validates :artist, presence: true
 
-  has_many :videos
+  has_many :videos, counter_cache: :count_of_songs
 
   # active admin scopes
   scope :sort_by_popularity,   -> { order(popularity: :desc) }
