62179304cfa7db86609a0a72defb0671ec8e8477
Jmarsh24 <jmarsh24@gmail.com>
2021-03-09 17:18:16 +0100

    added . to describe scope

--- spec/support/models/shared_examples/full_namable.rb
+++ spec/support/models/shared_examples/full_namable.rb
@@ -13,34 +13,34 @@ RSpec.shared_examples "a full nameable" do
     end
   end
 
-  describe "#abrev_name_nospace" do
+  describe ".abrev_name_nospace" do
     it "returns first name initial and last name without space" do
       object = described_class.new(first_name: "John", last_name: "Doe")
       expect(object.abrev_name_nospace).to eq("J.Doe")
     end
   end
-  describe "#full_name_search" do
+  describe ".full_name_search" do
     it "finds a searched follower by name" do
       object = described_class.create(name: "John Doe", first_name: "John", last_name: "Doe")
       @result = described_class.full_name_search("John Doe")
       expect(@result).to include(object)
     end
   end
-  describe "#full_name_search" do
+  describe ".full_name_search" do
     it "finds a searched follower by ending of name" do
       object = described_class.create(name: "John Doe", first_name: "John", last_name: "Doe")
       @result = described_class.full_name_search("ohn Doe")
       expect(@result).to include(object)
     end
   end
-  describe "#full_name_search" do
+  describe ".full_name_search" do
     it "finds a searched follower by beginning of name" do
       object = described_class.create(name: "John Doe", first_name: "John", last_name: "Doe")
       @result = described_class.full_name_search("John D")
       expect(@result).to include(object)
     end
   end
-  describe "#full_name_search" do
+  describe ".full_name_search" do
     it "finds a searched follower by with case insensitivity" do
       object = described_class.create(name: "John Doe", first_name: "John", last_name: "Doe")
       @result = described_class.full_name_search("John Doe")
