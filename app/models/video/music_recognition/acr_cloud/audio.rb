0052f22cf3fc832088435ad00e58893bc8455997
Jmarsh24 <jmarsh24@gmail.com>
2021-03-10 12:50:19 +0100

    refactor audio.rb

--- app/models/video/music_recognition/acr_cloud/audio.rb
+++ app/models/video/music_recognition/acr_cloud/audio.rb
@@ -7,40 +7,41 @@ class Video::MusicRecognition::AcrCloud::Audio
 
   def initialize(youtube_id)
     @youtube_id = youtube_id
-    @youtube_video_audio = fetch_by_id(youtube_id)
-    @video = Video.find_by(youtube_id: youtube_id)
+    @youtube_video_audio_file = fetch_by_id(youtube_id)
   end
 
   def import
-    transcode
-    output_file_path
+    transcode_audio_file
   end
 
   private
 
-  def fetch_by_id(_youtube_id)
+  def fetch_by_id(youtube_id)
     YoutubeDL.download(
       "https://www.youtube.com/watch?v=#{@youtube_id}",
       { format: "140", output: "~/environment/data/audio/%(id)s.mp3" }
     )
   end
 
-  def audio_file_path
-    @youtube_video_audio.filename.to_s
+  def youtube_audio_file_path
+    @youtube_video_audio_file.filename.to_s
   end
 
   def output_file_path
-    calculate_time
-    audio_file_path.gsub(/.mp3/, "_#{@time_1}_#{@time_2}.mp3")
+    @output_file_path ||= youtube_audio_file_path.gsub(/.mp3/, "_#{time_1}_#{time_2}.mp3")
   end
 
-  def calculate_time
-    @time_1 = @youtube_video_audio.duration / 2
-    @time_2 = @time_1 + 20
+  def time_1
+    @time_1 ||= @youtube_video_audio_file.duration / 2
   end
 
-  def transcode
-    song = FFMPEG::Movie.new(audio_file_path)
-    song.transcode(output_file_path, { custom: %W[-ss #{@time_1} -to #{@time_2}] })
+  def time_2
+    @time_2 ||= time_1 + 20
+  end
+
+  def transcode_audio_file
+    song = FFMPEG::Movie.new(youtube_audio_file_path)
+    song.transcode(output_file_path, { custom: %W[-ss #{time_1} -to #{time_2}] })
+    output_file_path
   end
 end
