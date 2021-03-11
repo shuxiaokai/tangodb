d908fb011ff2a7169bc076f29057a147963fc902
Jmarsh24 <jmarsh24@gmail.com>
2021-03-11 22:28:31 +0100

    update acr_cloud_ to global variables

--- app/models/video/music_recognition/acr_cloud/client.rb
+++ app/models/video/music_recognition/acr_cloud/client.rb
@@ -1,12 +1,12 @@
 class Video::MusicRecognition::AcrCloud::Client
-  HTTP_METHOD = "POST".freeze
-  HTTP_URI = "/v1/identify".freeze
-  DATA_TYPE = "audio".freeze
-  SIGNATURE_VERSION = "1".freeze
-  TIMESTAMP = Time.now.utc.to_i.to_s.freeze
-  ACCESS_KEY = ENV["ACRCLOUD_ACCESS_KEY"]
-  ACCESS_SECRET = ENV["ACRCLOUD_SECRET_KEY"]
-  REQ_URL = "http://identify-eu-west-1.acrcloud.com/v1/identify".freeze
+  ACR_CLOUD_HTTP_METHOD = "POST".freeze
+  ACR_CLOUD_HTTP_URI = "/v1/identify".freeze
+  ACR_CLOUD_DATA_TYPE = "audio".freeze
+  ACR_CLOUD_SIGNATURE_VERSION = "1".freeze
+  ACR_CLOUD_TIMESTAMP = Time.now.utc.to_i.to_s.freeze
+  ACR_CLOUD_ACCESS_KEY = ENV["ACRCLOUD_ACCESS_KEY"]
+  ACR_CLOUD_ACCESS_SECRET = ENV["ACRCLOUD_SECRET_KEY"]
+  ACR_CLOUD_REQ_URL = "http://identify-eu-west-1.acrcloud.com/v1/identify".freeze
 
   class << self
     def send_audio(file_path)
@@ -34,12 +34,12 @@ class Video::MusicRecognition::AcrCloud::Client
   def body
     {
       sample:            sample_file,
-      access_key:        ACCESS_KEY,
-      data_type:         DATA_TYPE,
-      signature_version: SIGNATURE_VERSION,
+      access_key:        ACR_CLOUD_ACCESS_KEY,
+      data_type:         ACR_CLOUD_DATA_TYPE,
+      signature_version: ACR_CLOUD_SIGNATURE_VERSION,
       signature:         signature,
       sample_bytes:      sample_bytes,
-      timestamp:         TIMESTAMP
+      timestamp:         ACR_CLOUD_TIMESTAMP
     }
   end
 
@@ -56,7 +56,12 @@ class Video::MusicRecognition::AcrCloud::Client
   end
 
   def unsigned_string
-    "#{HTTP_METHOD}\n#{HTTP_URI}\n#{ACCESS_KEY}\n#{DATA_TYPE}\n#{SIGNATURE_VERSION}\n#{TIMESTAMP}"
+    "#{ACR_CLOUD_HTTP_METHOD}\n
+      #{ACR_CLOUD_HTTP_URI}\n
+      #{ACR_CLOUD_ACCESS_KEY}\n
+      #{ACR_CLOUD_DATA_TYPE}\n
+      #{ACR_CLOUD_SIGNATURE_VERSION}\n
+      #{ACR_CLOUD_TIMESTAMP}"
   end
 
   def digest
@@ -64,6 +69,6 @@ class Video::MusicRecognition::AcrCloud::Client
   end
 
   def signature
-    Base64.encode64(OpenSSL::HMAC.digest(digest, ACCESS_SECRET, unsigned_string)).strip
+    Base64.encode64(OpenSSL::HMAC.digest(digest, ACR_CLOUD_ACCESS_SECRET, unsigned_string)).strip
   end
 end
