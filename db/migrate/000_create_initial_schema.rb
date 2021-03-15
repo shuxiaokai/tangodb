c2347572fcf60229d724d4fb719009e3b0f16fbc
Jmarsh24 <jmarsh24@gmail.com>
2021-03-15 14:41:58 +0100

    remove extension migration from structure.sql

--- db/migrate/000_create_initial_schema.rb
+++ db/migrate/000_create_initial_schema.rb
@@ -11,9 +11,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-CREATE EXTENSION IF NOT EXISTS fuzzystrmatch WITH SCHEMA public;
-CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
-CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
 SET default_tablespace = '';
 SET default_table_access_method = heap;
 CREATE TABLE public.admin_users (
