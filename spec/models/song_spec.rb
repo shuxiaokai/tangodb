0fe1cb9915b5f770f230ec48899f60b9ef538858
Jmarsh24 <jmarsh24@gmail.com>
2021-03-15 16:50:41 +0100

    initialize pull request

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,7 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Song, type: :model do
+  let(:song) { build(:leader) }
+  it { is_expected.to validate_presence_of(:genre) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:artist) }
+
   it { is_expected.to have_many(:videos) }
   it { is_expected.to have_many(:leader).through(:videos) }
   it { is_expected.to have_many(:follower).through(:videos) }
+
+  describe ".sort_by_popularity" do
+    it "includes users with admin flag" do
+      admin = Song.create!(admin: true)
+      expect(User.admins).to include(admin)
+    end
 end
