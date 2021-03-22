0706c1b8eb0b70cd64f2ce03a43acd75450feb27
Jmarsh24 <jmarsh24@gmail.com>
2021-03-22 16:50:03 +0100

    remove unneccesary song attributes from factory

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -24,42 +24,6 @@ FactoryBot.define do
     genre { "TANGO" }
     title { "La mentirosa" }
     artist { "Anibal Troilo" }
-    artist_2 { "Jorge Casal" }
-    composer { "Anselmo Aieta" }
-    author { "Francisco García Jiménez" }
-    date { "MyText" }
-    popularity { "0" }
-    active { true }
-    lyrics do
-      "Cuanto te amé,
-              puedo decir que jamás otra mujer,
-              podré querer como a vos. La juventud no volverá nunca más y a la ambición ya puedo dar el adiós. Qué tiempo aquel,
-              hora fugaz que pasó,
-              todo el valor de una pasión conocí. Cuanta feliz frase de amor escuché,
-              que siempre yo,
-              sumiso y fiel te creí.  Las caricias de tus manos,
-              tus palabras de ternura,
-              dejaron cruel amargura,
-              porque nada fue verdad. Besos falsos de tu boca,
-              juramentos,
-              ilusiones,
-              mataron mis ambiciones,
-              sin un poco de piedad.  Pero,
-              por el mal que vos me hiciste,
-              solo dice mi alma triste,
-              mentirosa,
-              mentirosa. Todo lo que me has hecho pasar,
-              penas,
-              llanto,
-              con otro lo has de pagar.  Ya encontrarás quien un amor fingirá entonces sí,
-              vas querer sin mentir,
-              has de ser vos la que al final llorará. Siempre de mi te acordarás al sufrir,
-              ha de sangrar tu corazón al pensar,
-              en todo el mal que hiciste a mi ilusión y hasta al morir,
-              hasta el morir,
-              mirarás los ojos del fantasma de tu traición."
-    end
-    last_name_search { "TROILO" }
   end
 
   factory :random_song do
--- spec/models/videos_search_spec.rb
+++ spec/models/videos_search_spec.rb
@@ -8,4 +8,19 @@
 require "rails_helper"
 
 RSpec.describe VideosSearch, type: :model do
+  describe ".refresh" do
+    it "materialized view able to be refreshed" do
+      video = create(:video)
+      VideosSearch.refresh
+      expect(VideosSearch.count).to eq(1)
+    end
+  end
+
+  describe ".search" do
+    it "materialized view is searchable" do
+      video = create(:video)
+      VideosSearch.refresh
+      expect(VideosSearch.count).to include(1)
+    end
+  end
 end
