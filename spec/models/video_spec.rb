39d0c5f74ddc303e08c1cada970771b6b7e5ef74
Jmarsh24 <jmarsh24@gmail.com>
2021-03-24 12:45:38 +0100

    add pagination

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -786,5 +786,17 @@ RSpec.describe Video, type: :model do
         expect(partial_match_result).to include(video)
       end
     end
+
+    describe ".paginate" do
+      it "limits pagination to 120 queries" do
+        create_list(:video, 200)
+        page1 = described_class.paginate(1, 120)
+        page2 = described_class.paginate(2, 120)
+        page3 = described_class.paginate(3, 120)
+        expect(page1.count).to eq(120)
+        expect(page2.count).to eq(80)
+        expect(page3.count).to eq(0)
+      end
+    end
   end
 end
