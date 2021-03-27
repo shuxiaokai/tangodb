6776e9ddaf61625a8997178e17257f98176b9eab
Jmarsh24 <jmarsh24@gmail.com>
2021-03-27 12:38:37 +0100

    initialize client spec

--- spec/models/video/music_recognition/acr_cloud/client_spec.rb
+++ spec/models/video/music_recognition/acr_cloud/client_spec.rb
@@ -1,5 +1,30 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Video::MusicRecognition::AcrCloud::Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".send_audio" do
+  end
+
+  describe "#get_audio_from_acr_cloud" do
+  end
+
+  describe "#body" do
+  end
+
+  describe "#sample_file" do
+  end
+
+  describe "#url" do
+  end
+
+  describe "sample_bytes" do
+  end
+
+  describe "unsigned_string" do
+  end
+
+  describe "digest" do
+  end
+
+  describe "signature" do
+  end
 end
