d7c935460c905f99e23919ae5b3fbdcc34248f39
Jmarsh24 <jmarsh24@gmail.com>
2021-03-27 12:46:37 +0100

    initialize video_spec

--- spec/models/video/youtube_import/video_spec.rb
+++ spec/models/video/youtube_import/video_spec.rb
@@ -1,5 +1,26 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Video::YoutubeImport::Video, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".import" do
+  end
+
+  describe "#import" do
+  end
+
+  describe "fetch_by_id" do
+  end
+
+  describe "to_video_params" do
+  end
+
+  describe "base_params" do
+  end
+
+  describe "count_params" do
+  end
+
+  describe "channel" do
+    youtube_video =
+      expect
+  end
 end
