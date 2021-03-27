814904611250e2a0f68d5a4ae99c26cdd79ca545
Jmarsh24 <jmarsh24@gmail.com>
2021-03-27 14:40:22 +0100

    initialize channel spec

--- spec/models/video/youtube_import/channel_spec.rb
+++ spec/models/video/youtube_import/channel_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Video::YoutubeImport::Channel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#import" do
+    it "receives response from youtube" do
+      channel = create(:channel, channel_id: "UCtdgMR0bmogczrZNpPaO66Q")
+      channel_id = channel.channel_id
+      expect(described_class.import(channel_id)).to be true
+    end
+  end
 end
