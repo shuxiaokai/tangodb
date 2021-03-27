bfa92a60b16974a0e0f8b89b737e973383626252
Jmarsh24 <jmarsh24@gmail.com>
2021-03-27 08:26:42 +0100

    consolidate song test

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -35,79 +35,63 @@ RSpec.describe Song, type: :model do
     it { is_expected.to have_many(:follower).through(:videos) }
   end
 
-  describe ".sort_by_popularity" do
-    it "order songs in database in descending order" do
-      popular_song = create(:song, popularity: 100)
-      less_popular_song = create(:song, popularity: 99)
-      expect(described_class.sort_by_popularity).to eq([popular_song, less_popular_song])
+  describe "scope" do
+    describe ".sort_by_popularity" do
+      it "order songs in database in descending order" do
+        popular_song = create(:song, popularity: 100)
+        less_popular_song = create(:song, popularity: 99)
+        expect(described_class.sort_by_popularity).to eq([popular_song, less_popular_song])
+      end
+    end
+
+    describe ".filter_by_active" do
+      it "returns active songs" do
+        active_song = create(:song, active: true)
+        not_active_song = create(:song, active: false)
+        expect(described_class.filter_by_active).to eq [active_song]
+        expect(described_class.filter_by_active).not_to include(not_active_song)
+      end
+    end
+
+    describe ".filter_by_not_active" do
+      it "returns not active songs" do
+        active_song = create(:song, active: true)
+        not_active_song = create(:song, active: false)
+        expect(described_class.filter_by_active).to eq [active_song]
+        expect(described_class.filter_by_not_active).to include(not_active_song)
+      end
+    end
+
+    describe ".title_match" do
+      it "find song by title" do
+        match_song = create(:song, title: "No Vendrá")
+        no_match_song = create(:song, title: "Not matching song")
+        expect(described_class.title_match("no vendra")).to include(match_song)
+        expect(described_class.title_match("no vendra")).not_to include(no_match_song)
+        expect(described_class.title_match("No Ven")).to include(match_song)
+        expect(described_class.title_match("No Ven")).not_to include(no_match_song)
+        expect(described_class.title_match("endra")).to include(match_song)
+        expect(described_class.title_match("endra")).not_to include(no_match_song)
+        expect(described_class.title_match("vend")).to include(match_song)
+        expect(described_class.title_match("vend")).not_to include(no_match_song)
+        expect(described_class.title_match("No Vendra")).to include(match_song)
+        expect(described_class.title_match("No Vendra")).not_to include(no_match_song)
+        expect(described_class.title_match("no vendra")).to include(match_song)
+        expect(described_class.title_match("no vendra")).not_to include(no_match_song)
+      end
     end
   end
 
-  describe ".filter_by_active" do
-    it "returns active songs" do
-      active_song = create(:song, active: true)
-      not_active_song = create(:song, active: false)
-      expect(described_class.filter_by_active).to include(active_song)
-      expect(described_class.filter_by_active).not_to include(not_active_song)
-    end
-  end
-
-  describe ".filter_by_not_active" do
-    it "returns not active songs" do
-      active_song = create(:song, active: true)
-      not_active_song = create(:song, active: false)
-      expect(described_class.filter_by_not_active).not_to include(active_song)
-      expect(described_class.filter_by_not_active).to include(not_active_song)
-    end
-  end
-
-  describe ".title_match" do
-    it "find song by title" do
-      match_song = create(:song, title: "No Vendrá")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.title_match("no vendra")
-      expect(result).to include(match_song)
-      expect(result).not_to include(no_match_song)
+  describe "#full_title" do
+    it "find song return string with 'title - artist - genre'" do
+      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
+      not_match_song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
+      expect(song.full_title).to eq("No Vendrá - Angel D'Agostino - Tango")
     end
 
-    it "find song by title with partial prefix" do
-      match_song = create(:song, title: "No Vendrá")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.title_match("No Ven")
-      expect(result).to include(match_song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song by title with partial suffix" do
-      match_song = create(:song, title: "No Vendrá")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.title_match("endra")
-      expect(result).to include(match_song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song by title with partial middle match" do
-      match_song = create(:song, title: "No Vendrá")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.title_match("vend")
-      expect(result).to include(match_song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song by title without accent" do
-      match_song = create(:song, title: "No Vendrá")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.title_match("No Vendra")
-      expect(result).to include(match_song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song by title without titleize" do
-      match_song = create(:song, title: "No Vendrá")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.title_match("no vendra")
-      expect(result).to include(match_song)
-      expect(result).not_to include(no_match_song)
+    it "titleizes artist names with apostrophe properly" do
+      song = create(:song, title: "Tal vez será su voz", artist: "Anibal Troilo", genre: "TANGO")
+      expect(song.full_title).to eq("Tal Vez Será Su Voz - Anibal Troilo - Tango")
     end
   end
 
@@ -115,77 +99,22 @@ RSpec.describe Song, type: :model do
     it "find song with artist" do
       song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
       no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("angel d'agostino")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song with partial artist" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("agostino")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song with genre" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("tango")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song with partial genre" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("ango")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song with title" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("no vendra")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "find song with partial title" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("endra")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "finds song with mixed multi word query" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("agostino vendra")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-
-    it "finds song with without matching apostophe" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      no_match_song = create(:song, title: "Not matching song")
-      result = described_class.full_title_search("dagostino")
-      expect(result).to include(song)
-      expect(result).not_to include(no_match_song)
-    end
-  end
-
-  describe ".full_title" do
-    it "find song return string with 'title - artist - genre'" do
-      song = create(:song, title: "No Vendrá", artist: "Angel D'AGOSTINO", genre: "TANGO")
-      expect(song.full_title).to eq("No Vendrá - Angel D'Agostino - Tango")
-    end
-
-    it "titleizes artist names with apostrophe properly" do
-      song = create(:song, title: "Tal vez será su voz", artist: "Anibal Troilo", genre: "TANGO")
-      expect(song.full_title).to eq("Tal Vez Será Su Voz - Anibal Troilo - Tango")
+      expect(described_class.full_title_search("angel d'agostino")).to include(song)
+      expect(described_class.full_title_search("angel d'agostino")).not_to include(no_match_song)
+      expect(described_class.full_title_search("agostino")).to include(song)
+      expect(described_class.full_title_search("agostino")).not_to include(no_match_song)
+      expect(described_class.full_title_search("tango")).to include(song)
+      expect(described_class.full_title_search("tango")).not_to include(no_match_song)
+      expect(described_class.full_title_search("ango")).to include(song)
+      expect(described_class.full_title_search("ango")).not_to include(no_match_song)
+      expect(described_class.full_title_search("no vendra")).to include(song)
+      expect(described_class.full_title_search("no vendra")).not_to include(no_match_song)
+      expect(described_class.full_title_search("endra")).to include(song)
+      expect(described_class.full_title_search("endra")).not_to include(no_match_song)
+      expect(described_class.full_title_search("agostino vendra")).to include(song)
+      expect(described_class.full_title_search("agostino vendra")).not_to include(no_match_song)
+      expect(described_class.full_title_search("dagostino")).to include(song)
+      expect(described_class.full_title_search("dagostino")).not_to include(no_match_song)
     end
   end
 end
