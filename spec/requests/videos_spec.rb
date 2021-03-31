ac82410f5cc38e63e4d2bdd01013dc006b8514ad
Jmarsh24 <jmarsh24@gmail.com>
2021-03-31 10:57:54 +0200

    add request specs

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -1,9 +1,30 @@
 require "rails_helper"
 
 RSpec.describe "Videos", type: :request do
-  describe "GET /videos" do
-    it "works! (now write some real specs)" do
+  describe "GET #index" do
+    it "succesfully returns get request" do
       get videos_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET #new" do
+    it "succesfully returns get request" do
+      get new_video_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET #new" do
+    it "succesfully returns get request" do
+      get new_video_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET #watch" do
+    it "succesfully returns get request" do
+      get watch_path, params: { v: "btQ5cluaCQM" }
       expect(response).to have_http_status(:ok)
     end
   end
