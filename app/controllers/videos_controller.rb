41fc46803f9eb6a5fa831b6115237e0aa601c1e1
Jmarsh24 <jmarsh24@gmail.com>
2021-03-31 15:20:07 +0200

    Revert "initialize facet filter with occurence count"
    This reverts commit 81fe94039c4c9361954731d14b9696f331b6721e.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,12 @@
 class VideosController < ApplicationController
   before_action :authenticate_user!, only: %i[edit update]
   before_action :current_search, only: %i[index show]
-  before_action :set_video, only: %i[update edit]
-  before_action :set_recommended_videos, only: %i[edit]
-
   NUMBER_OF_VIDEOS_PER_PAGE = 120
 
   helper_method :sort_column, :sort_direction
 
   def index
+    @videos_total = Video.not_hidden.size
     @videos = Video.not_hidden
                    .includes(:leader, :follower, :channel, :song, :event)
                    .order("#{sort_column} #{sort_direction}")
@@ -20,11 +18,11 @@ class VideosController < ApplicationController
     @next_page_items = @videos.paginate(page + 1, NUMBER_OF_VIDEOS_PER_PAGE)
     @items_display_count = (@videos.size - (@videos.size - (page * NUMBER_OF_VIDEOS_PER_PAGE).clamp(0, @videos.size)))
 
-    @leaders = facet("leaders.name", :leader)
-    @followers = facet("followers.name", :follower)
-    @channels = facet("channels.title", :channel)
-    @artists = facet("songs.artist", :song)
-    @genres = facet("songs.genre", :song)
+    @leaders    = @videos.joins(:leader).pluck("leaders.name").uniq.sort.map(&:titleize)
+    @followers  = @videos.joins(:follower).pluck("followers.name").uniq.sort.map(&:titleize)
+    @channels   = @videos.joins(:channel).pluck("channels.title").uniq.compact.sort
+    @artists    = @videos.joins(:song).pluck("songs.artist").uniq.compact.sort.map(&:titleize)
+    @genres     = @videos.joins(:song).pluck("songs.genre").uniq.compact.sort.map(&:titleize).uniq
 
     respond_to do |format|
       format.html
@@ -37,37 +35,23 @@ class VideosController < ApplicationController
 
   def show
     @video = Video.find_by(youtube_id: params[:v])
-    set_recommended_videos
+    @videos_total = Video.not_hidden.size
+    videos = if Video.where(song_id: @video.song_id).size > 3
+               Video.where(song_id: @video.song_id)
+             else
+               Video.where(channel_id: @video.channel_id)
+             end
+
+    @recommended_videos = videos.where(hidden: false)
+                                .where.not(youtube_id: @video.youtube_id)
+                                .order("popularity DESC")
+                                .limit(3)
     @video.clicked!
   end
 
-  def update
-    @video.update(video_params)
-    redirect_to watch_path(v: @video.youtube_id)
-  end
-
-  def create
-    @video = Video.create(youtube_id: params[:video][:youtube_id])
-    fetch_new_video
-
-    redirect_to root_path, notice: "Video Sucessfully Added: The video must be approved before the videos are added"
-  end
-
-  private
-
-  def facet(table_column, model)
-    query = "#{table_column} AS facet_value, count(#{table_column}) AS occurrences"
-    counts = Video.joins(model).select(query).group(table_column)
-    facet = counts.map do |c|
-      ["#{c.facet_value.titleize} (#{c.occurrences})", c.facet_value.titleize]
-    end
-  end
-
-  def set_video
+  def edit
     @video = Video.find(params[:id])
-  end
-
-  def set_recommended_videos
+    @videos_total = Video.not_hidden.size
     videos = if Video.where(song_id: @video.song_id).size > 3
                Video.where(song_id: @video.song_id)
              else
@@ -79,6 +63,14 @@ class VideosController < ApplicationController
                                 .order("popularity DESC")
                                 .limit(3)
   end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(video_params)
+    redirect_to watch_path(v: @video.youtube_id)
+  end
+
+  private
 
   def current_search
     @current_search = params[:query]
@@ -113,9 +105,5 @@ class VideosController < ApplicationController
 
   def filtering_params
     params.permit(:leader, :follower, :channel, :genre, :orchestra, :song_id, :query, :hd, :event_id)
-  end
-
-  def fetch_new_video
-    ImportVideoWorker.perform_async(@video.youtube_id)
   end
 end
