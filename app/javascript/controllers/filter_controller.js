1861d52f2833b1d47840658367d5f2a35907dbd9
jmarsh24 <48605824+jmarsh24@users.noreply.github.com>
2021-04-25 08:42:16 -0700

    fix pagination bug

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,56 +1,56 @@
-// filters_controller.js
-import { Controller } from "stimulus"
-import Rails from "@rails/ujs"
+import { Controller } from 'stimulus'
+import Rails from '@rails/ujs'
 
 export default class extends Controller {
-  static targets = ["filter"]
+  static targets = ['filter']
 
-  filter() {
-    const url = `${window.location.pathname}?${this.params}`;
+  filter () {
+    const url = `${window.location.pathname}?${this.params}`
 
     Rails.ajax({
-     type: "get",
-     url: url,
-     success: (data) => {
-      const newContainerFilters = data.getElementById('filter-container-upper')
-      const containerFilters = document.getElementById('filter-container-upper')
-      const newContainerVideos = data.getElementById('videos')
-      const containerVideos = document.getElementById('videos')
-      const newContainerLoadmore = data.getElementById('load-more-container')
-      const containerLoadmore = document.getElementById('load-more-container')
-      const newContainerFilterresults = data.getElementById('filter_results')
-      const containerFilterresults = document.getElementById('filter_results')
-      const containerSorting = document.getElementById('sortable_container')
-      const newContainerSorting = data.getElementById('sortable_container')
-      const containerHd = document.getElementById('hd_filters')
-      const newContainerHd = data.getElementById('hd_filters')
+      type: 'get',
+      url: url,
+      success: data => {
+        const newContainerFilters = data.getElementById(
+          'filter-container-upper'
+        )
+        const containerFilters = document.getElementById(
+          'filter-container-upper'
+        )
+        const newContainerVideos = data.getElementById('videos')
+        const containerVideos = document.getElementById('videos')
+        const newContainerFilterresults = data.getElementById('filter_results')
+        const containerFilterresults = document.getElementById('filter_results')
+        const containerSorting = document.getElementById('sortable_container')
+        const newContainerSorting = data.getElementById('sortable_container')
+        const containerHd = document.getElementById('hd_filters')
+        const newContainerHd = data.getElementById('hd_filters')
 
-      containerFilters.innerHTML = newContainerFilters.innerHTML
-      containerVideos.innerHTML = newContainerVideos.innerHTML
-      containerLoadmore.innerHTML = newContainerLoadmore.innerHTML
-      containerFilterresults.innerHTML = newContainerFilterresults.innerHTML
-      containerSorting.innerHTML = newContainerSorting.innerHTML
-      containerHd.innerHTML = newContainerHd.innerHTML
+        containerFilters.innerHTML = newContainerFilters.innerHTML
+        containerVideos.innerHTML = newContainerVideos.innerHTML
+        containerFilterresults.innerHTML = newContainerFilterresults.innerHTML
+        containerSorting.innerHTML = newContainerSorting.innerHTML
+        containerHd.innerHTML = newContainerHd.innerHTML
 
-      history.pushState({}, '', `${window.location.pathname}?${this.params}`)
-     },
-     error: (data) => {
-       console.log(data)
-     }
-   })
+        history.pushState({}, '', `${window.location.pathname}?${this.params}`)
+      },
+      error: data => {
+        console.log(data)
+      }
+    })
   }
 
-  get params() {
+  get params () {
     const queryString = window.location.search
     let searchParams = new URLSearchParams(queryString)
 
-      this.setCurrentParams(searchParams)
-      this.deleteEmptyParams(searchParams)
+    this.setCurrentParams(searchParams)
+    this.deleteEmptyParams(searchParams)
 
     return searchParams.toString()
   }
 
-  setCurrentParams(searchParams) {
+  setCurrentParams (searchParams) {
     let params = this.filterTargets.map(t => [t.name, t.value])
 
     params.forEach(param => searchParams.set(param[0], param[1]))
@@ -58,14 +58,14 @@ export default class extends Controller {
     return searchParams
   }
 
-  deleteEmptyParams(searchParams) {
+  deleteEmptyParams (searchParams) {
     let keysForDel = []
-      searchParams.forEach((v, k) => {
-        if (v == '' || k == '') keysForDel.push(k)
-      })
-      keysForDel.forEach(k => {
-        searchParams.delete(k)
-      })
-      return searchParams
+    searchParams.forEach((v, k) => {
+      if (v == '' || k == '') keysForDel.push(k)
+    })
+    keysForDel.forEach(k => {
+      searchParams.delete(k)
+    })
+    return searchParams
   }
 }
