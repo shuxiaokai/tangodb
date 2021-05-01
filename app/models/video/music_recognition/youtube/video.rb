b4b943486e9de54bc91cbd251e231366e300de9f
jmarsh24 <48605824+jmarsh24@users.noreply.github.com>
2021-04-30 17:25:12 -0700

    add edge case if internal video not found

--- app/models/video/music_recognition/youtube/video.rb
+++ app/models/video/music_recognition/youtube/video.rb
@@ -1,40 +1,42 @@
 class Video::MusicRecognition::Youtube::Video
-  YOUTUBE_DL_COMMAND_PREFIX = "https://www.youtube.com/watch?v=".freeze
-  YOUTUBE_DL_COMMAND_SUFFIX = "--skip-download".freeze
+  YOUTUBE_DL_COMMAND_PREFIX = "youtube-dl https://www.youtube.com/watch?v=".freeze
+  YOUTUBE_DL_COMMAND_SUFFIX = " --skip-download --print-json".freeze
 
   class << self
-    def import(video_id)
-      new(video_id).import
+    def import(youtube_id)
+      new(youtube_id).import
     end
   end
 
   def initialize(youtube_id)
     @youtube_id = youtube_id
-    @youtube_video = fetch_by_id(youtube_id)
     @video = Video.find_by(youtube_id: youtube_id)
   end
 
   def import
-    @video.update(video_params)
+    if @video.present?
+      @video.update(video_params)
+    else
+      video_params
+    end
   end
 
   private
 
-  def fetch_by_id
-    `#{YOUTUBE_DL_COMMAND_PREFIX + @youtube_id + YOUTUBE_DL_COMMAND_SUFFIX}`.split
+  def fetch_youtube_video_info_by_id
+    `#{YOUTUBE_DL_COMMAND_PREFIX + @youtube_id + YOUTUBE_DL_COMMAND_SUFFIX}`
     rescue StandardError => e
-      Rails.logger.warn "Video::MusicRecognition::Youtube::Video youtube-dl video fetching error: #{e.backtrace.join("\n\t")}"
-      ""
+    Rails.logger.warn("Video::MusicRecognition::Youtube::Video youtube-dl video fetching error: #{e.backtrace.join("\n\t")}")
   end
 
   def parsed_response
-    JSON.parse(@youtube_video.to_json).extend Hashie::Extensions::DeepFind
+    JSON.parse(fetch_youtube_video_info_by_id)
   end
 
   def video_params
     {
-      youtube_song: parsed_response.deep_find("track"),
-      youtube_artist: parsed_response.deep_find("artist")
+      youtube_song: parsed_response["track"],
+      youtube_artist: parsed_response["artist"]
     }
   end
 end
