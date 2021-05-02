54938effbc9f8f2365a086ddb7081ae168c668c2
jmarsh24 <48605824+jmarsh24@users.noreply.github.com>
2021-05-01 18:08:26 -0700

    refactor filter controller

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,22 +1,19 @@
-import { Controller } from "stimulus"
-import Rails from "@rails/ujs"
+import { Controller } from 'stimulus'
+import Rails from '@rails/ujs'
 import { Turbo } from '@hotwired/turbo-rails'
 
 export default class extends Controller {
-  static targets = [ "filter" ]
-  static values = { sort: String,
-                    direction: String,
-                    hd: String
-                  }
+  static targets = ['filter']
+  static values = { sort: String, direction: String, hd: String }
 
-  filter() {
+  filter () {
     const url = `${window.location.pathname}?${this.params}`
 
     this.getBack()
     this.replaceContents(url)
   }
 
-  get params() {
+  get params () {
     const queryString = window.location.search
     let searchParams = new URLSearchParams(queryString)
 
@@ -26,54 +23,53 @@ export default class extends Controller {
     return searchParams.toString()
   }
 
-  setCurrentParams(searchParams) {
+  setCurrentParams (searchParams) {
     let params = this.filterTargets.map(t => [t.name, t.value])
 
-    let sortParam = ["sort", this.sortValue]
-    let directionParam = ["direction", this.directionValue]
-    let hdParam = ["hd", this.hdValue]
+    let sortParam = ['sort', this.sortValue]
+    let directionParam = ['direction', this.directionValue]
+    let hdParam = ['hd', this.hdValue]
+    let extraParams = [sortParam, directionParam, hdParam]
 
-    if (sortParam[1]) {
-      searchParams.set(sortParam[0], sortParam[1])
-    }
-
-    if (directionParam[1]) {
-      searchParams.set(directionParam[0], directionParam[1])
-    }
-
-    if (hdParam[1]) {
-      searchParams.set(hdParam[0], hdParam[1])
-    }
+    extraParams.forEach(element => {
+      if (element[1]) {
+        searchParams.set(element[0], element[1])
+      }
+    })
 
     params.forEach(param => searchParams.set(param[0], param[1]))
 
     return searchParams
   }
 
-  deleteEmptyParams(searchParams) {
+  deleteEmptyParams (searchParams) {
     let keysForDel = []
-      searchParams.forEach((v, k) => {
-        if (v == '' || k == '' || v == '0') keysForDel.push(k)
-      })
-      keysForDel.forEach(k => {
-        searchParams.delete(k)
-      })
-      return searchParams
+    searchParams.forEach((v, k) => {
+      if (v == '' || k == '' || v == '0') keysForDel.push(k)
+    })
+    keysForDel.forEach(k => {
+      searchParams.delete(k)
+    })
+    return searchParams
   }
 
-  replaceContents(url) {
+  replaceContents (url) {
     Rails.ajax({
       type: 'get',
       url: url,
       success: data => {
-        const replaceContainers = ['filter-container-upper',
-                                    'videos',
-                                    'load-more-container',
-                                    'filter_results',
-                                    'sortable_container']
+        const replaceContainers = [
+          'filter-container-upper',
+          'videos',
+          'load-more-container',
+          'filter_results',
+          'sortable_container'
+        ]
 
-        replaceContainers.forEach( element => {
-          document.getElementById(element).innerHTML = data.getElementById(element).innerHTML
+        replaceContainers.forEach(element => {
+          document.getElementById(element).innerHTML = data.getElementById(
+            element
+          ).innerHTML
         })
 
         history.pushState({}, '', `${window.location.pathname}?${this.params}`)
@@ -84,7 +80,7 @@ export default class extends Controller {
     })
   }
 
-  getBack() {
+  getBack () {
     window.onpopstate = function () {
       Turbo.visit(document.location)
     }
