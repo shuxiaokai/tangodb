f3a19bd620edba68b05347234f287d4bfaf3ad14
jmarsh24 <48605824+jmarsh24@users.noreply.github.com>
2021-05-01 18:00:46 -0700

    refactored filter controller

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,4 +1,3 @@
-// filters_controller.js
 import { Controller } from "stimulus"
 import Rails from "@rails/ujs"
 import { Turbo } from '@hotwired/turbo-rails'
@@ -13,45 +12,16 @@ export default class extends Controller {
   filter() {
     const url = `${window.location.pathname}?${this.params}`
 
-    Rails.ajax({
-     type: "get",
-     url: url,
-     success: (data) => {
-
-        const newContainerFilters = data.getElementById('filter-container-upper')
-        const containerFilters = document.getElementById('filter-container-upper')
-        const newContainerVideos = data.getElementById('videos')
-        const containerVideos = document.getElementById('videos')
-        const newContainerLoadmore = data.getElementById('load-more-container')
-        const containerLoadmore = document.getElementById('load-more-container')
-        const newContainerFilterresults = data.getElementById('filter_results')
-        const containerFilterresults = document.getElementById('filter_results')
-        const containerSorting = document.getElementById('sortable_container')
-        const newContainerSorting = data.getElementById('sortable_container')
-
-        containerFilters.innerHTML = newContainerFilters.innerHTML
-        containerVideos.innerHTML = newContainerVideos.innerHTML
-        containerLoadmore.innerHTML = newContainerLoadmore.innerHTML
-        containerFilterresults.innerHTML = newContainerFilterresults.innerHTML
-        containerSorting.innerHTML = newContainerSorting.innerHTML
-
-        history.pushState({}, '', `${window.location.pathname}?${this.params}`)
-        window.onpopstate = function () {
-          Turbo.visit(document.location)
-        }
-     },
-     error: (data) => {
-       console.log(data)
-     }
-   })
+    this.getBack()
+    this.replaceContents(url)
   }
 
   get params() {
     const queryString = window.location.search
     let searchParams = new URLSearchParams(queryString)
 
-      this.setCurrentParams(searchParams)
-      this.deleteEmptyParams(searchParams)
+    this.setCurrentParams(searchParams)
+    this.deleteEmptyParams(searchParams)
 
     return searchParams.toString()
   }
@@ -89,5 +59,34 @@ export default class extends Controller {
         searchParams.delete(k)
       })
       return searchParams
+  }
+
+  replaceContents(url) {
+    Rails.ajax({
+      type: 'get',
+      url: url,
+      success: data => {
+        const replaceContainers = ['filter-container-upper',
+                                    'videos',
+                                    'load-more-container',
+                                    'filter_results',
+                                    'sortable_container']
+
+        replaceContainers.forEach( element => {
+          document.getElementById(element).innerHTML = data.getElementById(element).innerHTML
+        })
+
+        history.pushState({}, '', `${window.location.pathname}?${this.params}`)
+      },
+      error: data => {
+        console.log(data)
+      }
+    })
+  }
+
+  getBack() {
+    window.onpopstate = function () {
+      Turbo.visit(document.location)
+    }
   }
 }
